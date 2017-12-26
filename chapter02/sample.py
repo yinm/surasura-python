2c531d476c7ac45493d39aafaecc5977143c735f
yinm <yinm.cminor9@gmail.com>
2017-12-26 22:53:25 +0900

    Check practice

--- chapter02/sample.py
+++ chapter02/sample.py
@@ -1,3 +1,15 @@
-print('hello'.replace('e', 'o'))
-print('hello'.replace('e', ''))
-print('hello'.replace('l', ''))
+print(10 + 5)
+print(10 / 3)
+print(3 * (5 + 1))
+
+print(10 // 3)
+print(10 % 3)
+
+print('10' + '5')
+
+number = 10
+print(number + 5)
+
+print(str(number) + '5')
+
+print('hello'.replace('h', 'c'))
