d7c73dfd0241b964a98b7ffc76af4af688802afc
yinm <yinm.cminor9@gmail.com>
2017-12-27 08:15:54 +0900

    Check practice

--- chapter03/sample.py
+++ chapter03/sample.py
@@ -1,3 +1,25 @@
-member = 5
-today = '水'
-print(member <= 4 or today != '月')
+print(5 < 3)
+print(5 > 3)
+
+if 5 > 3:
+    print('5 > 3はTrue')
+if 5 < 3:
+    print('5 < 3はTrue?')
+
+number = 5
+if number == 5:
+    print('numberと5は等しい')
+
+number = 6
+if number != 5:
+    print('numberと5は等しくない')
+
+number1 = 5
+number2 = 10
+if number1 == 5 and number2 == 10:
+    print('and条件クリア')
+
+number1 = 5
+number2 = 15
+if number1 != 5 or number2 == 15:
+    print('or条件クリア')
