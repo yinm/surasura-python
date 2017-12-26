af2d5541318745b9c643d08ba1406a8b1bb64c01
yinm <yinm.cminor9@gmail.com>
2017-12-27 08:08:10 +0900

    Check nest

--- chapter03/sample.py
+++ chapter03/sample.py
@@ -1,11 +1,11 @@
-member = 3
-if member == 1:
-    store = 'A'
-elif member == 2:
-    store = 'B'
-elif member <= 4:
-    store = 'C'
+member = 4
+today = '月'
+if member >= 4:
+    if today != '月':
+        store = 'A'
+    else:
+        store = 'B'
 else:
-    store = 'D'
+    store = 'C'
 
 print('{}の店に行く'.format(store))
