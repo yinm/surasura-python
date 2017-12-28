0118c3f5494ad2cc7546f6bccc33a521c09e0f99
yinm <yinm.cminor9@gmail.com>
2017-12-28 19:36:52 +0900

    Check for and if 3

--- chapter04/sample.py
+++ chapter04/sample.py
@@ -1,8 +1,12 @@
 numbers = [1, 2, 3, 4, 5, 6]
 odd_list = []
+even_list = []
 
 for number in numbers:
     if number % 2 == 1:
         odd_list.append(number)
+    elif number % 2 == 0:
+        even_list.append(number)
 
 print(odd_list)
+print(even_list)
