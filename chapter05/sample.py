a472bdf61c0eba7666ccd4254752d9cedeecfbf4
yinm <yinm.cminor9@gmail.com>
2017-12-29 08:20:11 +0900

    Check dictionary.values()

--- chapter05/sample.py
+++ chapter05/sample.py
@@ -1,9 +1,4 @@
 fruits_dict = {'apple': 100, 'orange': 150}
-if 'apple' in fruits_dict:
-    print('appleはfruitsの中にあります')
-
-if 'tomato' not in fruits_dict:
-    print('tomatoはfruitsの中にありません')
-
-if 100 not in fruits_dict:
-    print('100はfruitsのkeyにはありません')
+print(fruits_dict.values())
+if 100 in fruits_dict.values():
+    print('100はfruitsのvaluesにあります')
