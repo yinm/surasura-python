a29c63b2302da676fa93c85ee156bb35f9498039
yinm <yinm.cminor9@gmail.com>
2017-12-29 08:37:47 +0900

    Check practice questions

--- chapter05/sample.py
+++ chapter05/sample.py
@@ -1,5 +1,12 @@
-fruits_dict = {'apple': 100, 'orange': 150, 'banana': 200}
-for fruit_item in fruits_dict.items():
-    key = fruit_item[0]
-    value = fruit_item[1]
+alpha_num_dict = {}
+
+alpha_num_dict = {'a': 1, 'b': 2, 'c': 3}
+
+print(alpha_num_dict)
+
+alpha_num_dict['a'] = 10
+print(alpha_num_dict['a'])
+
+alpha_num_dict = {'a': 1, 'b': 2, 'c': 3}
+for key, value in alpha_num_dict.items():
     print(key, value)
