372d0c5366ceb14b3ee8d17b86a252d14d75b55b
yinm <yinm.cminor9@gmail.com>
2017-12-29 08:53:05 +0900

    Check function

--- chapter06/sample.py
+++ chapter06/sample.py
@@ -2,5 +2,8 @@ def make_greet(name):
     greet = 'Hello {}-san'.format(name)
     return greet
 
-greet_result = make_greet('Yamada')
-print(greet_result)
+print(make_greet('Sato'))
+print(make_greet('Suzuki'))
+print(make_greet('Tanaka'))
+print('last one.')
+print(make_greet('Hasegawa'))
