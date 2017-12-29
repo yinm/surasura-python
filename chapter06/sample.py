3f3af8148cf78237efef6473f0e77b1c27397244
yinm <yinm.cminor9@gmail.com>
2017-12-29 09:04:38 +0900

    Check default value of arguments

--- chapter06/sample.py
+++ chapter06/sample.py
@@ -1,8 +1,5 @@
-num = 10
+def say(name = 'Taro'):
+    print(name)
 
-def make_num(num):
-    local_num = num + 10
-    print('ローカル変数は', local_num)
-
-make_num(num)
-print('ローカル変数外は', num)
+say('Hanako')
+say()
