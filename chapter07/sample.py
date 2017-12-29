8517e87628838530c5e668a718707820d3aac60d
yinm <yinm.cminor9@gmail.com>
2017-12-29 09:25:48 +0900

    Check try ~ except

--- chapter07/sample.py
+++ chapter07/sample.py
@@ -1,5 +1,10 @@
 def add_10(num):
-    add_num = int(num) + 10
-    return add_num
+    try:
+        add_num = num + 10
+        print('add_num is {}'.format(add_num))
+        return add_num
+    except:
+        print('Error!')
 
-print(add_10('10'))
+print(add_10(10))
+print(add_10('二十'))
