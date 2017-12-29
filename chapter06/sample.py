fa85ad0ff101a6878761369e89eefbf3ea359caf
yinm <yinm.cminor9@gmail.com>
2017-12-29 09:08:04 +0900

    Check practice questions

--- chapter06/sample.py
+++ chapter06/sample.py
@@ -1,5 +1,11 @@
-def say(name = 'Taro'):
-    print(name)
+def print_square():
+    print(3 * 3)
+print_square()
 
-say('Hanako')
-say()
+def print_square(number):
+    print(number * number)
+print_square(4)
+
+def calc_square(number):
+    return number * number
+print(calc_square(5))
